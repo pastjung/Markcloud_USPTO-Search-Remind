2239e32940351f7c40d1e5b1daf12f22447bfe56
정재현 <chwogus0303@naver.com>
2024-08-27 16:12:30 +0900

    feat(login_service): FastAPI & MariaDB 연동 확인 API 구현

--- login_service/service/main.py
+++ login_service/service/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 from .docs import *
 from config.database import Base, engine
+from sqlalchemy import text
 
 def get_server():
     server = FastAPI(
@@ -31,6 +32,20 @@ Base.metadata.create_all(bind=engine)
 # Fastapi 실행
 app = get_server()
 
-@app.get('/', tags=['Root'])
+@app.get('/ping', tags=['Root'])
 def ping():
     return 200
+
+@app.get("/mariadb/ping", tags=['Root'])
+async def mariadb_ping():
+    try:
+        # 데이터베이스 연결 테스트
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+            if result.scalar() == 1:
+                return 200
+            else:
+                raise HTTPException(status_code=500, detail="Unexpected result from database")
+    except Exception as e:
+        # 연결 오류 발생 시
+        raise HTTPException(status_code=500, detail=f"Database connection error: {str(e)}")
