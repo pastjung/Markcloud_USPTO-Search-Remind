ac5f2d14fb53ed6fb4e060ed0d2bb63185351831
정재현 <chwogus0303@naver.com>
2024-08-27 16:14:29 +0900

    feat(crawling_service): 초기세팅 & docker-compose.yml 설정 & FastAPI, MongoDB 연결 확인 API 구현

---
+++ crawling_service/config/database.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from starlette.config import Config
+
+# 환경 변수 불러오기
+config = Config('crawling_service/.env')
+db_port = config('MONGODB_SERVER_PORT')
+db_name = config('MONGODB_DB_NAME')
+collection_name = config('MONGODB_COLLECTION_NAME')
+
+client = MongoClient(f'mongodb://crawling_db:{db_port}/')
+db = client[db_name]
+collection = db[collection_name]
+
---
+++ crawling_service/service/docs.py
@@ -0,0 +1,12 @@
+root_description="""
+Crawling Service
+
+## 주의사항
+
+"""
+
+tag_meta = [
+    {
+        "name": "Root",
+    }
+]
