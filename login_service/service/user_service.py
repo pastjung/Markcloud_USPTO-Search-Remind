e3bb06d74d3cf2e21220997f48e18dc5f3fc79f4
정재현 <chwogus0303@naver.com>
2024-08-29 17:09:17 +0900

    chore(user_service.py): user_repository 명시

--- login_service/service/user_service.py
+++ login_service/service/user_service.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 
 from schema.user_schema import UserSignup, UserSignin, UserInfo
 from model.user_model import User, UserRole
-from repository.user_repository import send_query
+from repository import user_repository
 
 pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,7 +13,7 @@ def user_signup(user:UserSignup, connection):
     
     # 계정 생성
     sql = f'INSERT INTO user (user_id, nickname, password, birth, role) VALUES ("{user.user_id}", "{user.nickname}", "{pwd.hash(user.password)}", "{user.birth}", "{UserRole.user.value}")'
-    send_query(sql, connection, "update")
+    user_repository.send_query(sql, connection, "update")
     
 def user_signin(user:UserSignin, connection):
     None
@@ -23,7 +23,7 @@ def user_logout(user:UserInfo, connection):
     
 def user_read(user:UserInfo, connection) -> User:
     sql = f'SELECT * FROM user WHERE user_id = "{user.user_id}"'
-    result = send_query(sql, connection, "single")
+    result = user_repository.send_query(sql, connection, "single")
     return result
     
 def user_read_all(user:UserInfo, connection) -> list[User]:
@@ -33,7 +33,7 @@ def user_read_all(user:UserInfo, connection) -> list[User]:
         raise HTTPException(status_code=401, detail="Unauthorized : 관리자만 접근 가능합니다")
     
     sql = f'SELECT * FROM user'
-    return send_query(sql, connection, "all")
+    return user_repository.send_query(sql, connection, "all")
     
 def user_update(user:UserInfo, user_update:UserSignup, connection):
     db_user = user_read(user, connection)
@@ -42,13 +42,13 @@ def user_update(user:UserInfo, user_update:UserSignup, connection):
         verify_user_id(user_update.user_id, connection)
         
     sql = f'UPDATE user SET user_id = "{user_update.user_id}", nickname = "{user_update.nickname}", password = "{pwd.hash(user_update.password)}", birth = "{user_update.birth}" WHERE id = "{db_user.id}"'
-    send_query(sql, connection, "update")
+    user_repository.send_query(sql, connection, "update")
     
 def user_delete(user:UserInfo, connection):
     db_user = user_read(user, connection)
     
     sql = f'DELETE FROM user WHERE id = "{db_user.id}"'
-    send_query(sql, connection, "update")
+    user_repository.send_query(sql, connection, "update")
     
 def user_role_admin(user:UserInfo, connection):
     db_user = user_read(user, connection)
@@ -57,7 +57,7 @@ def user_role_admin(user:UserInfo, connection):
         raise HTTPException(status_code=400, detail="이미 관리자입니다.")
     
     sql = f'UPDATE user SET role = "{UserRole.admin.value}" where id = "{db_user.id}"'
-    send_query(sql, connection, "update")
+    user_repository.send_query(sql, connection, "update")
     
 def user_role_user(user:UserInfo, connection):
     db_user = user_read(user, connection)
@@ -66,13 +66,13 @@ def user_role_user(user:UserInfo, connection):
         raise HTTPException(status_code=400, detail="이미 사용자입니다.")
     
     sql = f'UPDATE user SET role = "{UserRole.user.value}" where id = "{db_user.id}"'
-    send_query(sql, connection, "update")
+    user_repository.send_query(sql, connection, "update")
     
 
 # DB에 user_id이 존재하는지 확인
 def verify_user_id(user_id: str, connection):
     sql = f'SELECT COUNT(*) as count FROM user WHERE user_id = "{user_id}"'
-    result = send_query(sql, connection, "single")
+    result = user_repository.send_query(sql, connection, "single")
     
     if result[0] > 0:
         raise HTTPException(status_code=409, detail="이미 존재하는 사용자 이름입니다.")
